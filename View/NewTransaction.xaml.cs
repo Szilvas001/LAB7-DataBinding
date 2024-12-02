9df1b06e149d259d00a0695de82ba3dd4f9137c2
Szilvas001 <75503366+Szilvas001@users.noreply.github.com>
2024-12-02 01:19:08 +0100

    Update NewTransaction.xaml.cs

--- View/NewTransaction.xaml.cs
+++ View/NewTransaction.xaml.cs
@@ -1,20 +1,60 @@
 using LAB09_MAUI_DataBindingLab.Model;
+using Microsoft.Maui.Controls;
+using System;
 using System.ComponentModel;
-using LAB09_MAUI_DataBindingLab.ViewModel;
+using System.Linq;
 
 namespace LAB09_MAUI_DataBindingLab.View
 {
-
-	public partial class NewTransaction : ContentView
-	{
+    public partial class NewTransaction : ContentView, INotifyPropertyChanged
+    {
         private TransactionList Transactions;
-
         public CategoryList Categories { get; private set; }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         #region Values entered into the form
-        public int SelectedCategoryIndex { get; set; } = 0;
-        public string Description { get; set; }
-        public int Value { get; set; }
+        private int selectedCategoryIndex = 0;
+        public int SelectedCategoryIndex
+        {
+            get => selectedCategoryIndex;
+            set
+            {
+                if (selectedCategoryIndex != value)
+                {
+                    selectedCategoryIndex = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string description = string.Empty;
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int value = 0;
+        public int Value
+        {
+            get => value;
+            set
+            {
+                if (value != value)
+                {
+                    this.value = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         #endregion
 
         public NewTransaction()
@@ -33,6 +73,28 @@ namespace LAB09_MAUI_DataBindingLab.View
 
         private void AddTransactionClicked(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(Description) || Value == 0)
+            {
+                return;
+            }
+
+            var transaction = new Transaction
+            {
+                Description = Description,
+                Value = Value,
+                Category = Categories[SelectedCategoryIndex]
+            };
+
+            Transactions.Add(transaction);
+
+            Description = string.Empty;
+            Value = 0;
+            SelectedCategoryIndex = 0;
+        }
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
